415469db47c7e10343813aa6f3228f3d48b0d4f3
Yuri Petrosyan <yuriyuri200117@gmail.com>
2025-10-08 19:04:15 +0200

    add wikipedia tool

--- tools.py
+++ tools.py
@@ -12,3 +12,6 @@ search_tool = Tool(
     func = search.run,
     description="Search the web for information"
 )
+
+api_wrapper = WikipediaAPIWrapper(top_k_results=3, doc_content_chars_max=100)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
