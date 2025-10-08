43c8066f3922ee0d0808f209414996e60432dc76
Yuri Petrosyan <yuriyuri200117@gmail.com>
2025-10-08 18:44:59 +0200

    add search tools

---
+++ tools.py
@@ -0,0 +1,5 @@
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+
+from dateTime import datetime
